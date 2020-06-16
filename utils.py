e4d25f38a08a002338103329fb46c975bce925ec
Md Nazrul Islam <connect2nazrul@gmail.com>
2020-06-16 19:52:43 +0600

    :sparkels: factory function has been included in init template.

--- utils.py
+++ utils.py
@@ -5,10 +5,41 @@ import fhirrenderer
 
 __author__ = "Md Nazrul Islam <email2nazrul@gmail.com>"
 
+INIT_TPL = """
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from typing import Any, Dict, Union
+
+from .fhirabstractmodel import FHIRAbstractModel
+from .fhirtypesvalidators import get_fhir_model_class
+
+__fhir_version__ = "{0}"
+
+
+def construct_fhir_element(
+    element_type: str, data: Union[Dict[str, Any], str, bytes, Path]
+) -> FHIRAbstractModel:
+    """ """
+    try:
+        klass = get_fhir_model_class(element_type)
+    except KeyError:
+        raise LookupError(
+            f"{element_type} doesnt to be valid FHIRModel (element type) name!"
+        )
+    if isinstance(data, (str, bytes)):
+        return klass.parse_raw(data, content_type="application/json")
+    elif isinstance(data, Path):
+        return klass.parse_file(data)
+    return klass.parse_obj(data)
+
+
+__all__ = ["get_fhir_model_class", "construct_fhir_element"]
+"""
+
 
 def ensure_init_py(settings, version_info):
     """ """
-    init_tpl = """# -*- coding: utf-8 -*-\n\n__fhir_version__ = "{0}"\n""".format(
+    init_tpl = INIT_TPL.format(
         version_info.version
     )
 
